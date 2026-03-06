a578710abd357f0e3a7e2c455f12f09110faac10
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2026-03-06 10:08:14 -0500

    Added the lab

---
+++ Labs/Machine Learning Interatomic Potentials/Lab 4 - 1/gem-butene-temps/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=gem_temps
+#SBATCH --time=24:00:00
+#SBATCH --ntasks=4
+#SBATCH --mem-per-cpu=2000
+#SBATCH --output=orca_test-%j.out
+
+# Load ORCA module (check what's available with: module avail orca)
+module load orca/6.0.0
+
+# Run ORCA
+$(which orca) gem.inp > gem.out
